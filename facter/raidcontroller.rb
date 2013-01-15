a52493b9f035a86d176585ad6d0e7712ef96c4c7
Jonas Genannt <jonas@brachium-system.net>
2013-01-15 15:23:28 +0100

    added MegaRAID SAS 2008 for MegaCli

--- facter/raidcontroller.rb
+++ facter/raidcontroller.rb
@@ -8,7 +8,7 @@ Facter.add("raidcontroller") do
 			output = %x{/usr/bin/lspci}
 			output.split(/\n/).each do |line|
 				controllers.push("sas2ircu") if line =~ /SAS2008/
-				controllers.push("megaraid") if line =~ /(MegaRAID SAS 1078|MegaSAS 9260|MegaRAID SAS 9240|MegaRAID SAS 2208)/
+				controllers.push("megaraid") if line =~ /(MegaRAID SAS 1078|MegaSAS 9260|MegaRAID SAS 9240|MegaRAID SAS 2208|MegaRAID SAS 2008)/
 				controllers.push("3ware") if line =~ /3ware Inc 9690SA/
 				controllers.push("aac-raid") if line =~ /Adaptec AAC-RAID/
 			end
